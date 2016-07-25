c47a5c087b8425836bf38f2486447185ffa353ce
Taylor Moore <Tmoore2272@gmail.com>
2016-07-25 15:33:51 -0600

    Hello World

---
+++ index.ios.js
@@ -0,0 +1,24 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+class PropertyFinderApp extends Component {
+  render() {
+    return React.createElement(Text, {style: styles.text}, "Hello World!");
+  }
+}
+
+AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
